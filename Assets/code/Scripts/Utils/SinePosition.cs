155c0d75964b45708e3471a685b5a7690111b01f
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2024-08-29 21:06:42 +0700

    feat: sine position

---
+++ Assets/code/Scripts/Utils/SinePosition.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinePosition : MonoBehaviour
+{
+    [SerializeField] float _period = 1;
+    
+    [Header("False to use y")]
+    [SerializeField]
+    bool _useX = true;
+    [SerializeField] float _amplitude = 100;
+
+    Vector3 _initialPosition;
+
+    void Start()
+    {
+        _initialPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float t = Time.time / _period;
+        float sine = Mathf.Sin(t * Mathf.PI * 2);
+        Vector3 position = _initialPosition;
+        if (_useX)
+        {
+            position.x += sine * _amplitude;
+        }
+        else
+        {
+            position.y += sine * _amplitude;
+        }
+        transform.position = position;
+    }
+}
