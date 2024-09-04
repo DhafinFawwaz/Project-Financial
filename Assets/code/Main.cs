a15569b1508b4c96a1b68fcaa6376f651b18bb54
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2024-09-05 00:49:22 +0700

    feat: some budgeting

--- Assets/code/Main.cs
+++ Assets/code/Main.cs
@@ -8,6 +8,7 @@ public class Main
         Singleton.Initialize();
         ResolutionManager.Initialize();
         ResolutionManager.SetResolution(PlayerPrefs.GetInt("Resolution", 3));
+        Audio.SetSoundMixerVolume(0);
         Save.Initialize();
     }
 }
