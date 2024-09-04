ad3e14de77969b8a8c7a6afd1e1897c81669b260
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2024-09-05 00:11:15 +0700

    feat: more dialogs

--- Assets/code/Scripts/NPC/NPC.cs
+++ Assets/code/Scripts/NPC/NPC.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 
 public class NPC : Interactable
 {
-    [SerializeField] DialogData _commentList;
+    [SerializeField] DialogData _commentListJustGotHome;
+    [SerializeField] DialogData _commentListAfterStreaming;
+    [SerializeField] DialogData _commentListAfterSleeping;
+    [SerializeField] DialogData _commentListJustGotOutside;
     [SerializeField] DialogData _commentListAfterKuliah;
+    [SerializeField] DialogData _commentListAfterBudgeting;
     [SerializeField] DialogData _commentListAfterBelanja;
+
     public static Action<DialogData> s_OnNPCInteract;
 
     protected override void OnPlayerInteract()
     {
         base.OnPlayerInteract();
-        if(Save.Data.DayState == DayState.AfterKuliah && _commentListAfterKuliah) s_OnNPCInteract?.Invoke(_commentListAfterKuliah);
-        else if(Save.Data.DayState == DayState.AfterBelanja && _commentListAfterBelanja) s_OnNPCInteract?.Invoke(_commentListAfterBelanja);
-        else if(Save.Data.DayState == DayState.JustGotOutside && _commentList) s_OnNPCInteract?.Invoke(_commentList);
+        switch(Save.Data.DayState) {
+            case DayState.JustGotHome: s_OnNPCInteract?.Invoke(_commentListJustGotHome); break;
+            case DayState.AfterStreaming: s_OnNPCInteract?.Invoke(_commentListAfterStreaming); break;
+            case DayState.AfterSleeping: s_OnNPCInteract?.Invoke(_commentListAfterSleeping); break;
+            case DayState.JustGotOutside: s_OnNPCInteract?.Invoke(_commentListJustGotOutside); break;
+            case DayState.AfterKuliah: s_OnNPCInteract?.Invoke(_commentListAfterKuliah); break;
+            case DayState.AfterBudgeting: s_OnNPCInteract?.Invoke(_commentListAfterBudgeting); break;
+            case DayState.AfterBelanja: s_OnNPCInteract?.Invoke(_commentListAfterBelanja); break;
+        }
     }
 }
