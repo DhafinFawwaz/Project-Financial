31c8e53ac4e79732daa3814e6411a8c32b4ec700
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2024-09-06 05:26:12 +0700

    feat: belanja tutorial day 3

--- Assets/code/Scripts/Belanja/FirstTimeBelanja.cs
+++ Assets/code/Scripts/Belanja/FirstTimeBelanja.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class FirstTimeBelanja : MonoBehaviour
 {
     [SerializeField] PopUp _popUp;
+    [SerializeField] PopUp _popUpRil;
     [SerializeField] BelanjaManager _belanjaManager;
     void Start()
     {
         if(Save.Data.CurrentDay == 1) {
             _popUp.Show();
+        } else if(Save.Data.CurrentDay >= 2) {
+            _popUpRil.Show();
         } else {
             _belanjaManager.gameObject.SetActive(true);
             _belanjaManager.StartGame();
@@ -19,6 +22,12 @@ public class FirstTimeBelanja : MonoBehaviour
     public void ClosePopUp()
     {
         _popUp.Hide();
+        _belanjaManager.gameObject.SetActive(true);
+        _belanjaManager.StartGame();
+    }
+    public void ClosePopUpRil()
+    {
+        _popUpRil.Hide();
         _belanjaManager.gameObject.SetActive(true);
         _belanjaManager.StartGame();
     }
